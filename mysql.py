80e9c06ad712a6ca2ecac8fa13c2915e5fa6229c
oneplum7 <2104878583@qq.com>
2020-11-18 00:21:19 +0800

    second commit

--- mysql.py
+++ mysql.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -:-*- coding: utf-8 -*-
 # author:           oneplum7
 # create_time:      2020/10
 # file_name:        rightview.py
@@ -171,9 +171,3 @@ ide ="id"
 values5 = "date_time"
 values6 = "rank_code"
 print(ide,values5,values6)
-
-
-
-
-
-    
